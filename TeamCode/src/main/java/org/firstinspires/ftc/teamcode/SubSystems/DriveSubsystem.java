2aaa4034d5f8e8a3e24fe5de62df9a1681076e4d
AvivDukhovich <avivaviv11223344@gmail.com>
2025-10-02 00:01:29 +0300

    aviv: added an option for headingOffset in DriveSubsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/DriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/DriveSubsystem.java
@@ -4,39 +4,28 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
-import com.seattlesolvers.solverslib.drivebase.MecanumDrive;
-import com.seattlesolvers.solverslib.hardware.motors.Motor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class DriveSubsystem extends SubsystemBase {
 
     public IMU imu;
-    public final MecanumDriveComponent drive;
+    public double headingOffset;
 
-    // Define hub mounting orientation
+    public final MecanumDriveComponent mecanumDriveComponent;
+
     RevHubOrientationOnRobot.LogoFacingDirection logoFacingDirection =
             RevHubOrientationOnRobot.LogoFacingDirection.UP;       // Change if logo is not facing up
     RevHubOrientationOnRobot.UsbFacingDirection usbFacingDirection =
             RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;   // Change if USB is not facing forward
 
-    // Create IMU parameters using hub orientation
     IMU.Parameters imuParameters = new IMU.Parameters(
             new RevHubOrientationOnRobot(logoFacingDirection, usbFacingDirection)
     );
 
-//    private final Encoder m_left, m_right;
-//
-//    private final double WHEEL_DIAMETER;
 
-    /**
-     * Creates a new DriveSubsystem.
-     */
     public DriveSubsystem(HardwareMap hw) {
-//        m_left = leftMotor.encoder;
-//        m_right = rightMotor.encoder;
 
-//        WHEEL_DIAMETER = diameter;
-        drive = new MecanumDriveComponent(
+        mecanumDriveComponent = new MecanumDriveComponent(
                 hw.get(DcMotorEx.class, "leftFront"),
                 hw.get(DcMotorEx.class, "leftBack"),
                 hw.get(DcMotorEx.class, "rightFront"),
@@ -45,39 +34,33 @@ public class DriveSubsystem extends SubsystemBase {
 
         imu = hw.get(IMU.class, "imu");
         imu.initialize(imuParameters);
+
+        headingOffset = 0;
+    }
+    public DriveSubsystem(HardwareMap hw, double headingOffset) {
+
+        mecanumDriveComponent = new MecanumDriveComponent(
+                hw.get(DcMotorEx.class, "leftFront"),
+                hw.get(DcMotorEx.class, "leftBack"),
+                hw.get(DcMotorEx.class, "rightFront"),
+                hw.get(DcMotorEx.class, "rightBack")
+        );
+
+        imu = hw.get(IMU.class, "imu");
+        imu.initialize(imuParameters);
+
+        this.headingOffset = headingOffset;
     }
 
+
+
+
     public double getHeading(){
-        return imu.getRobotYawPitchRollAngles().getYaw();
+        return imu.getRobotYawPitchRollAngles().getYaw() + headingOffset;
     }
 
     public void drive(double x, double y, double turn) {
-        drive.driveFieldCentric(x, y, turn, getHeading());
+        mecanumDriveComponent.driveFieldCentric(x, y, turn, getHeading());
     }
-
-//    public double getLeftEncoderVal() {
-//        return m_left.getPosition();
-//    }
-//
-//    public double getLeftEncoderDistance() {
-//        return m_left.getRevolutions() * WHEEL_DIAMETER * Math.PI;
-//    }
-
-//    public double getRightEncoderVal() {
-//        return m_right.getPosition();
-//    }
-//
-//    public double getRightEncoderDistance() {
-//        return m_right.getRevolutions() * WHEEL_DIAMETER * Math.PI;
-//    }
-
-//    public void resetEncoders() {
-//        m_left.reset();
-//        m_right.reset();
-//    }
-//
-//    public double getAverageEncoderDistance() {
-//        return (getLeftEncoderDistance() + getRightEncoderDistance()) / 2.0;
-//    }
 
 }

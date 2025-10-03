7d27eee85f3f587c49db1805ec5bffb17cc227f8
BarnYard22993 <barnyard22993@gmail.com>
2025-10-03 16:35:14 +0300

    egor - added Tranfer tester teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP/TestTransTeleop.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.TeleOP;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.seattlesolvers.solverslib.command.CommandOpMode;
+import com.seattlesolvers.solverslib.command.button.Button;
+import com.seattlesolvers.solverslib.command.button.GamepadButton;
+import com.seattlesolvers.solverslib.gamepad.GamepadEx;
+import com.seattlesolvers.solverslib.gamepad.GamepadKeys;
+
+import org.firstinspires.ftc.teamcode.Commands.TransferArtifactCommand;
+import org.firstinspires.ftc.teamcode.SubSystems.Transfer;
+
+@TeleOp
+public class TestTransTeleop extends CommandOpMode {
+    private Transfer transfer;
+    private GamepadEx gamepadEx1;
+
+    private Button startTransferButton;
+
+    private TransferArtifactCommand transferCommand;
+
+
+    @Override
+    public void initialize() {
+        gamepadEx1 = new GamepadEx(gamepad1);
+        transfer = new Transfer(hardwareMap);
+        startTransferButton = new GamepadButton(gamepadEx1, GamepadKeys.Button.Y).whenPressed(transferCommand);
+        register(transfer);
+
+
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        telemetry.addData("Controller input : ", startTransferButton.get() ? 1 : 0);
+        telemetry.update();
+    }
+}

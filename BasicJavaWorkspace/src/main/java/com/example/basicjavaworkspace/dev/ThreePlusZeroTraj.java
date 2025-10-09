08e6af5248d1c89a55475448094f88405cb5c176
Natanchok <natanmarkch@gmail.com>
2025-10-09 20:20:36 +0300

    made first auto

--- BasicJavaWorkspace/src/main/java/com/example/basicjavaworkspace/dev/ThreePlusZeroTraj.java
+++ BasicJavaWorkspace/src/main/java/com/example/basicjavaworkspace/dev/ThreePlusZeroTraj.java
@@ -1,28 +1,33 @@
 package com.example.basicjavaworkspace.dev;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Rotation2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
+// The class name now matches the original filename
 public class ThreePlusZeroTraj {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(80, 80, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-50, 20, 0))
-                .lineToX(30)
-                .turn(Math.toRadians(90))
-                .lineToY(30)
-                .turn(Math.toRadians(90))
-                .lineToX(0)
-                .turn(Math.toRadians(90))
-                .lineToY(0)
-                .turn(Math.toRadians(90))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-37, -53, Math.toRadians(90)))
+                .strafeToLinearHeading(new Vector2d(-46, -23), 180)
+                .waitSeconds(1)
+                .strafeToLinearHeading(new Vector2d(-12, -23), Math.toRadians(270))
+                .strafeToLinearHeading(new Vector2d(-11, -51), Math.toRadians(270))
+                .strafeToLinearHeading(new Vector2d(-46, -23), 180)
+                .waitSeconds(1)
+                .strafeToLinearHeading(new Vector2d(10.6, -23.1), Math.toRadians(270))
+                .strafeToLinearHeading(new Vector2d(10.6, -51), Math.toRadians(270))
+                .strafeToLinearHeading(new Vector2d(-46, -23), 180)
+
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_OFFICIAL)

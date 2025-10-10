eea2ce63f3a1375da226f0e84839705c1114d273
AvivDukhovich <avivaviv11223344@gmail.com>
2025-10-10 14:10:03 +0300

    leon test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/leonTeleopTest.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.testing;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@TeleOp
+public class leonTeleopTest extends LinearOpMode {
+    DcMotorEx shooter, intake;
+    CRServo leftFront, leftBack, rightFront, rightBack;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        shooter = hardwareMap.get(DcMotorEx.class, "shooter");
+        intake = hardwareMap.get(DcMotorEx.class, "intake");
+        leftBack = hardwareMap.get(CRServo.class, "leftBack");
+        leftFront = hardwareMap.get(CRServo.class, "leftFront");
+        rightBack = hardwareMap.get(CRServo.class, "rightBack");
+        rightFront = hardwareMap.get(CRServo.class, "rightFront");
+
+        waitForStart();
+        while(opModeIsActive()){
+
+            shooter.setPower(0.5);
+            intake.setPower(0.5);
+            leftBack.setPower(1);
+            leftFront.setPower(1);
+            rightBack.setPower(1);
+            rightFront.setPower(1);
+        }
+    }
+}

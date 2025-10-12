2e6465e97fa07365ea15fe681d88843d3954e651
BarnYard22993 <barnyard22993@gmail.com>
2025-10-12 14:05:03 +0300

    lil addition to the systemTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/teleopSystemTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/teleopSystemTest.java
@@ -4,11 +4,10 @@ package org.firstinspires.ftc.teamcode.testing;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 @TeleOp
-public class leonTeleopTest extends LinearOpMode {
+public class teleopSystemTest extends LinearOpMode {
     DcMotorEx shooter, intake;
     CRServo leftFront, leftBack, rightFront, rightBack;
 
@@ -23,14 +22,26 @@ public class leonTeleopTest extends LinearOpMode {
         rightFront = hardwareMap.get(CRServo.class, "rightFront");
 
         waitForStart();
-        while(opModeIsActive()){
+        while (opModeIsActive()) {
 
             shooter.setPower(0.7);
             intake.setPower(1);
-            leftBack.setPower(1);
-            leftFront.setPower(1);
-            rightBack.setPower(1);
-            rightFront.setPower(1);
+
+            if (gamepad1.a) leftBack.setPower(1);
+            if (gamepad1.b) leftFront.setPower(1);
+            if (gamepad1.x) rightBack.setPower(1);
+            if (gamepad1.y) rightFront.setPower(1);
+
+            if (gamepad1.left_trigger > 0.5) shooter.setPower(gamepad1.left_trigger);
+            if (gamepad1.right_trigger > 0.5) intake.setPower(gamepad1.left_trigger);
+
+            telemetry.addData("leftBack a:", gamepad1.a);
+            telemetry.addData("leftFront b:", gamepad1.b);
+            telemetry.addData("rightBack x:", gamepad1.x);
+            telemetry.addData("rightFront y:", gamepad1.y);
+            telemetry.addData("intake rTrigger:", gamepad1.right_trigger);
+            telemetry.addData("shooter lTrigger:", gamepad1.left_trigger);
+
         }
     }
 }

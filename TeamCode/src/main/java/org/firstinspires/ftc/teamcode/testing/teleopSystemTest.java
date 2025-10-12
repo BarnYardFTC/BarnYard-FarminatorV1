e0b42eac1d38792c10d02b548df1bf776930a4f2
BarnYard22993 <barnyard22993@gmail.com>
2025-10-12 14:29:46 +0300

    lil addition to the systemTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/teleopSystemTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/teleopSystemTest.java
@@ -16,24 +16,28 @@ public class teleopSystemTest extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         shooter = hardwareMap.get(DcMotorEx.class, "shooter");
         intake = hardwareMap.get(DcMotorEx.class, "intake");
-        leftBack = hardwareMap.get(CRServo.class, "leftBack");
-        leftFront = hardwareMap.get(CRServo.class, "leftFront");
-        rightBack = hardwareMap.get(CRServo.class, "rightBack");
-        rightFront = hardwareMap.get(CRServo.class, "rightFront");
+        leftBack = hardwareMap.get(CRServo.class, "leftBackTransfer");
+        leftFront = hardwareMap.get(CRServo.class, "leftFrontTransfer");
+        rightBack = hardwareMap.get(CRServo.class, "rightBackTransfer");
+        rightFront = hardwareMap.get(CRServo.class, "rightFrontTransfer");
 
         waitForStart();
         while (opModeIsActive()) {
 
-            shooter.setPower(0.7);
-            intake.setPower(1);
 
             if (gamepad1.a) leftBack.setPower(1);
+            else leftBack.setPower(0);
             if (gamepad1.b) leftFront.setPower(1);
+            else leftFront.setPower(0);
             if (gamepad1.x) rightBack.setPower(1);
+            else rightBack.setPower(0);
             if (gamepad1.y) rightFront.setPower(1);
+            else rightFront.setPower(0);
 
             if (gamepad1.left_trigger > 0.5) shooter.setPower(gamepad1.left_trigger);
+            else   shooter.setPower(0);
             if (gamepad1.right_trigger > 0.5) intake.setPower(gamepad1.left_trigger);
+            else intake.setPower(0);
 
             telemetry.addData("leftBack a:", gamepad1.a);
             telemetry.addData("leftFront b:", gamepad1.b);

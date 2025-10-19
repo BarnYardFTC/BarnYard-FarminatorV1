a35db1a7c17d81f7ec1da05cf5984a0ef2404048
Barnyard22993 <barnyard22993@gmail.com>
2025-10-19 19:59:26 +0300

    Alona/Natan: finished meepmeepthreepluszero far trajectory

---
+++ BasicJavaWorkspace/src/main/java/com/example/basicjavaworkspace/meepmeep/close/ThreePlusThreeGPP.java
@@ -0,0 +1,41 @@
+package com.example.basicjavaworkspace.meepmeep.close;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class ThreePlusThreeGPP {
+
+
+    // The class name now matches the original filename
+
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-37, -53, Math.toRadians(90)))
+                .strafeToLinearHeading(new Vector2d(-15.5, -15.5), 180)
+                .waitSeconds(1)
+
+                .strafeToLinearHeading(new Vector2d(34, -23.1), Math.toRadians(270))
+                .strafeToLinearHeading(new Vector2d(34, -51), Math.toRadians(270))
+                .strafeToLinearHeading(new Vector2d(34, -35), Math.toRadians(270))
+
+                .strafeToLinearHeading(new Vector2d(-15.5, -15.5), 180)
+
+                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
+

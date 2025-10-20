a2cd509f24107f07028100a20ba560cd3a9fa0d2
BarnYard22993 <barnyard22993@gmail.com>
2025-10-20 16:16:36 +0300

    Aviv: Working on roadrunner tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/roadrunner/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/roadrunner/MecanumDrive.java
@@ -62,7 +62,7 @@ public final class MecanumDrive {
                 RevHubOrientationOnRobot.UsbFacingDirection.UP;
 
         // drive model parameters
-        public double inPerTick = 1;
+        public double inPerTick = 0.00500740645;
         public double lateralInPerTick = inPerTick;
         public double trackWidthTicks = 0;
 

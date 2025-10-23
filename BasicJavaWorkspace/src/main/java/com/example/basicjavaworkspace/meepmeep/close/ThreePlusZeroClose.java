b1cdb67689397a88b88660e9616f1a7bcd9b8f7a
Natanchok <natanmarkch@gmail.com>
2025-10-23 10:59:48 +0300

    did slow mode and refactor the autos so the look the same

--- BasicJavaWorkspace/src/main/java/com/example/basicjavaworkspace/meepmeep/close/ThreePlusZeroClose.java
+++ BasicJavaWorkspace/src/main/java/com/example/basicjavaworkspace/meepmeep/close/ThreePlusZeroClose.java
@@ -1,35 +1,36 @@
-package com.example.basicjavaworkspace.meepmeep.close;
+                                                                            package com.example.basicjavaworkspace.meepmeep.close;
 
-import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.Vector2d;
-import com.noahbres.meepmeep.MeepMeep;
-import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
-import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+                                                                            import com.acmerobotics.roadrunner.Pose2d;
+                                                                            import com.acmerobotics.roadrunner.Vector2d;
+                                                                            import com.noahbres.meepmeep.MeepMeep;
+                                                                            import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+                                                                            import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-public class ThreePlusZeroClose {
-
-
-    // The class name now matches the original filename
-
-    public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
-
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .build();
-
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-37, -53, Math.toRadians(90)))
-                .strafeToLinearHeading(new Vector2d(-15.5, -15.5), 180)
+                                                                            public class ThreePlusZeroClose {
 
 
-                .build());
+                                                                                // The class name now matches the original filename
 
-        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_OFFICIAL)
-                .setDarkMode(true)
-                .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
-                .start();
-    }
-}
+                                                                                public static void main(String[] args) {
+                                                                                    MeepMeep meepMeep = new MeepMeep(800);
+
+                                                                                    RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                                                                                            // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                                                                                            .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                                                                                            .setDimensions(18, 18)
+                                                                                            .build();
+
+                                                                                    myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-37, -53, Math.toRadians(90)))
+                                                                                            .strafeToLinearHeading(new Vector2d(-15.5, -15.5), 180)
+
+
+                                                                                            .build());
+
+                                                                                    meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_OFFICIAL)
+                                                                                            .setDarkMode(true)
+                                                                                            .setBackgroundAlpha(0.95f)
+                                                                                            .addEntity(myBot)
+                                                                                            .start();
+                                                                                }
+                                                                            }
 
--- BasicJavaWorkspace/src/main/java/com/example/basicjavaworkspace/meepmeep/far/ThreePLusThree/GGPfar.java
+++ BasicJavaWorkspace/src/main/java/com/example/basicjavaworkspace/meepmeep/far/ThreePLusThree/GGPfar.java
@@ -1,4 +1,53 @@
 package com.example.basicjavaworkspace.meepmeep.far.ThreePLusThree;
 
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
 public class GGPfar {
-}
+    public static double POSE1_X = 60;
+    public static double POSE1_Y = -8.8;
+    public static double POSE1_HEADING = Math.toRadians(180);
+
+    public static double POSE2_X = 57;
+    public static double POSE2_Y = -23;
+    public static double POSE2_HEADING = Math.toRadians(200);
+
+    public static double POSE3_X = 32;
+    public static double POSE3_Y = -22;
+    public static double POSE3_HEADING = Math.toRadians(180);
+
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setDimensions(13.157, 18.03044)
+                .build();
+
+        TrajectoryActionBuilder path1 = myBot.getDrive().actionBuilder(new Pose2d(POSE1_X, POSE1_Y, POSE1_HEADING))
+                .strafeToLinearHeading(new Vector2d(POSE2_X, POSE2_Y), POSE2_HEADING);
+
+        TrajectoryActionBuilder path2 = myBot.getDrive().actionBuilder(new Pose2d(POSE2_X, POSE2_Y, POSE2_HEADING))
+                .strafeToLinearHeading(new Vector2d(POSE3_X, POSE3_Y), POSE3_HEADING);
+
+        myBot.runAction(
+                new SequentialAction(
+                        path1.build(),
+                        new SleepAction(10),
+                        path2.build()
+                ));
+
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+}}
+

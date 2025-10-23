de024a421d5c6c4c74aabbec1a24c72158d9d9d1
BarnYard22993 <barnyard22993@gmail.com>
2025-10-23 18:04:49 +0300

    Sasha: working on threePlusZeroClose

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooter.java
@@ -46,7 +46,7 @@ public class Shooter  extends SubsystemBase {
     }
 
     public Command activateShooterCommand() {
-        return new InstantCommand(() -> shooter.setPower(1), this);
+        return new InstantCommand(() -> setSpeed(DEFAULT_SPEED), this);
     }
 
     public Command deactivateShooterCommand() {

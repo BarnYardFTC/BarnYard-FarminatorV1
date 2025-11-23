5c0be25c8731b1e6d74a0e1f90fca23d03b5bc09
AvivDukhovich <avivaviv11223344@gmail.com>
2025-11-23 14:14:05 +0200

    Sasha - tuned shoot when ready command

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandGroups/ShootSequenceCommandGroup.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandGroups/ShootSequenceCommandGroup.java
@@ -86,7 +86,6 @@ public class ShootSequenceCommandGroup extends SequentialCommandGroup {
                 robot.transfer.setFrontPowerCommand(0),
                 robot.intake.deactivateIntakeCommand(),
                 new WaitCommand(2000)
-
         );
     }
 

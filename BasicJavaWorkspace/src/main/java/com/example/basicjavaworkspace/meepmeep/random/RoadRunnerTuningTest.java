796860a2081252a4716f6d8c274acaab7e302f40
BarnYard22993 <barnyard22993@gmail.com>
2025-11-27 17:10:36 +0200

    Aviv: added roadrunner tuning test

---
+++ BasicJavaWorkspace/src/main/java/com/example/basicjavaworkspace/meepmeep/random/RoadRunnerTuningTest.java
@@ -0,0 +1,43 @@
+package com.example.basicjavaworkspace.meepmeep.random;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class RoadRunnerTuningTest {
+
+    public static void main(String[] args) {
+
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setDimensions(13.157, 18.03044)
+                .build();
+
+        // Run the trajectory
+        myBot.runAction(
+                myBot.getDrive().actionBuilder(new Pose2d(15, 0, Math.PI/2))
+                        .splineTo(new Vector2d(0, 15), Math.PI)
+                        .waitSeconds(0.5)
+                        .splineTo(new Vector2d(-15, 0), Math.PI*1.5)
+                        .waitSeconds(0.5)
+                        .splineTo(new Vector2d(0, -15), Math.PI*2)
+                        .waitSeconds(0.5)
+                        .splineTo(new Vector2d(15, 0), Math.PI/2)
+                        .waitSeconds(0.5)
+                        .build()
+        );
+
+        // MeepMeep visualization
+        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/RoadRunnerTuningTest.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode.testing;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.util.roadrunner.RoadRunnerMecanumDrive;
+import org.firstinspires.ftc.teamcode.util.roadrunner.TankDrive;
+import org.firstinspires.ftc.teamcode.util.roadrunner.tuning.TuningOpModes;
+
+@Autonomous(name="raodrunner tuning test", group="test")
+public class RoadRunnerTuningTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Pose2d beginPose = new Pose2d(15, 0, Math.PI/2);
+        if (TuningOpModes.DRIVE_CLASS.equals(RoadRunnerMecanumDrive.class)) {
+            RoadRunnerMecanumDrive drive = new RoadRunnerMecanumDrive(hardwareMap, beginPose);
+
+            waitForStart();
+
+            while (opModeIsActive()){
+                Actions.runBlocking(
+                        drive.actionBuilder(new Pose2d(10, 0, Math.PI/2))
+                                .splineTo(new Vector2d(0, 10), Math.PI)
+                                .waitSeconds(0.5)
+                                .splineTo(new Vector2d(-10, 0), Math.PI*1.5)
+                                .waitSeconds(0.5)
+                                .splineTo(new Vector2d(0, -10), Math.PI*2)
+                                .waitSeconds(0.5)
+                                .splineTo(new Vector2d(10, 0), Math.PI/2)
+                                .waitSeconds(0.5)
+                                .build()
+                );
+            }
+        } else if (TuningOpModes.DRIVE_CLASS.equals(TankDrive.class)) {
+            TankDrive drive = new TankDrive(hardwareMap, beginPose);
+
+            waitForStart();
+
+            Actions.runBlocking(
+                    drive.actionBuilder(beginPose)
+                            .splineTo(new Vector2d(30, 30), Math.PI / 2)
+                            .splineTo(new Vector2d(0, 60), Math.PI)
+                            .build());
+        } else {
+            throw new RuntimeException();
+        }
+    }
+}

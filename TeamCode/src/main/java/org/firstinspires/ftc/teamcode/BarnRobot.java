0572b03cf0397639697efacf649c984fe16b816b
Barnyard22993 <barnyard22993@gmail.com>
2025-12-17 21:22:13 +0200

    Aviv: Added threePlusThree auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BarnRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BarnRobot.java
@@ -142,7 +142,7 @@ public class BarnRobot extends Robot {
     /** Sets up the webcam. */
     public void initWebcam(HardwareMap hw){
         webcam = new Webcam(hw);
-        webcam.setDefaultCommand(webcam.operateCommand());
+//        webcam.setDefaultCommand(webcam.operateCommand());
     }
 
     /** Sets up the shooter system. */

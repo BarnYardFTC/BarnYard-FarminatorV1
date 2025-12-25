09b447478554e23d158d5e1e367d11e26d2625a0
Barnyard22993 <barnyard22993@gmail.com>
2025-12-25 17:10:02 +0200

    Aviv: Super little change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveTrain.java
@@ -205,8 +205,6 @@ public class DriveTrain extends SubsystemBase {
 
         double diffYaw = desiredHeading - currentHeading;
 
-        BarnRobot.getInstance().telemetry.addData("dyaw", diffYaw);
-        BarnRobot.getInstance().telemetry.addData("desired heading", desiredHeading);
         double turnSpd = diffToSpeed(diffYaw);
 
         if (BarnRobot.getInstance().opmodeData.opModeType == OpModeData.OpModeType.TELEOP){

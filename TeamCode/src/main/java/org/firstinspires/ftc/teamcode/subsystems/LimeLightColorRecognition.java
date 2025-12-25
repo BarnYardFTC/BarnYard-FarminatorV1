d47a2c7109d2e95d79a6f93a848e69d295758cb0
BarnYard22993 <barnyard22993@gmail.com>
2025-12-25 15:55:29 +0200

    Peter - moved limelight colorRecognition from dev-localization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LimeLightColorRecognition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LimeLightColorRecognition.java
@@ -1,4 +1,58 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+
+import org.firstinspires.ftc.teamcode.BarnRobot;
+
+import java.util.List;
+
 public class LimeLightColorRecognition {
+    private Limelight3A limelight;
+
+    /** Latest result from the Limelight. */
+    private LLResult llResult;
+
+    /** List of fiducial results from the Limelight. */
+    private List<LLResultTypes.FiducialResult> frs;
+
+    private double ta;
+    private double tx;
+    private double ty;
+    private boolean isFound = false;
+
+
+    public LimeLightColorRecognition() {
+        limelight = BarnRobot.getInstance().robotHardware.limelight;
+        limelight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
+        limelight.start(); // This tells Limelight to start looking!
+        limelight.pipelineSwitch(0); // Switch to pipeline number 0
+    }
+
+    public void llResult(){
+        LLResult result = limelight.getLatestResult();
+        if (result != null && result.isValid()) {
+            double tx = result.getTx(); // How far left or right the target is (degrees)
+            double ty = result.getTy(); // How far up or down the target is (degrees)
+            double ta = result.getTa(); // How big the target looks (0%-100% of the image)
+            isFound = true;
+        }
+        else {isFound = false;}
+    }
+
+    public double getTa(){
+        return ta;
+    }
+    public double getTx(){
+        return tx;
+    }
+    public double getTy(){
+        return ty;
+    }
+    public boolean getIsFound(){
+        return isFound;
+    }
+
+
 }

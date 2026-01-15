d9930fc4d18ad43833fae8da8366c22597e9e464
BarnYard22993 <barnyard22993@gmail.com>
2026-01-15 18:22:58 +0200

    Aviv: Shat on the shit on the keyboard (fuck you autistic nigger)

--- BasicJavaWorkspace/src/main/java/com/example/basicjavaworkspace/meepmeep/red/close/Red_Close_ThreePlusNine.java
+++ BasicJavaWorkspace/src/main/java/com/example/basicjavaworkspace/meepmeep/red/close/Red_Close_ThreePlusNine.java
@@ -13,36 +13,31 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class Red_Close_ThreePlusNine {
 
-
     public static long SHOOT_TIME = 2;
+
     public static double START_POSE_X = -53.333;
     public static double START_POSE_Y = 45.5;
     public static double START_HEADING = Math.toRadians(125);
 
     public static double SHOOT_POSE_X = -23;
     public static double SHOOT_POSE_Y = 22;
-    public static double SHOOT_HEADING = Math.toRadians(135);
+    public static double SHOOT_HEADING = Math.toRadians(129);
 
-    public static double NORTH_READY_POSE_Y = 22;
-    public static double NORTH_COLLECT_POSE_Y = 53;
-    public static double NORTH_HEADING = Math.toRadians(90);
+    public static double SOUTH_READY_POSE_Y = -30;
+    public static double SOUTH_COLLECT_POSE_Y = 53;
 
-    public static double LEFT_COLLECT_POSE_X = -11;
-    public static double MID_COLLECT_POSE_X = 11.8;
+    public static double SOUTH_HEADING = Math.toRadians(90);
+
+    public static double LEFT_COLLECT_POSE_X = -11.5;
+    public static double MID_COLLECT_POSE_X = 12;
     public static double RIGHT_COLLECT_POSE_X = 35;
 
-    public static double GATE_POSE_Y = 44;
-
     public static double GATE_POSE_X = 0;
+    public static double GATE_POSE_Y = 44;
 
     public static double ENDING_POSE_X = -40;
     public static double ENDING_POSE_Y = 22;
 
-    public static double defaultVel = 50;
-
-    // -----------------------------
-    // Main Simulation
-    // -----------------------------
     public static void main(String[] args) {
 
         MeepMeep meepMeep = new MeepMeep(800);
@@ -55,11 +50,23 @@ public class Red_Close_ThreePlusNine {
         TrajectoryActionBuilder path1;
         path1 = myBot.getDrive().actionBuilder(
                         new Pose2d(START_POSE_X, START_POSE_Y, START_HEADING))
-                .strafeToLinearHeading(new Vector2d(START_POSE_X, START_POSE_Y-5), NORTH_HEADING)
-                .splineToConstantHeading(new Vector2d(LEFT_COLLECT_POSE_X, NORTH_COLLECT_POSE_Y), new Rotation2d(-0.2,2))
+                .strafeToLinearHeading(new Vector2d(START_POSE_X, START_POSE_Y-5), SOUTH_HEADING)
+                .splineToConstantHeading(new Vector2d(LEFT_COLLECT_POSE_X, SOUTH_COLLECT_POSE_Y), new Rotation2d(0,4))
+
                 .splineToConstantHeading(
-                        new Vector2d(GATE_POSE_X, NORTH_COLLECT_POSE_Y),
-                        new Rotation2d(0, 8)
+                        new Vector2d(GATE_POSE_X, SOUTH_COLLECT_POSE_Y),
+                        new Rotation2d(0, 8))
+                .strafeToLinearHeading(
+                        new Vector2d(SHOOT_POSE_X, SHOOT_POSE_Y),
+                        SHOOT_HEADING, new TranslationalVelConstraint(100)
+                );
+
+
+        TrajectoryActionBuilder path3 = myBot.getDrive().actionBuilder(
+                        new Pose2d(LEFT_COLLECT_POSE_X, SOUTH_COLLECT_POSE_Y, SOUTH_HEADING))
+                .splineToConstantHeading(
+                        new Vector2d(GATE_POSE_X, SOUTH_COLLECT_POSE_Y),
+                        new Rotation2d(-3, 8)
                 )
 
                 .strafeToLinearHeading(
@@ -67,32 +74,43 @@ public class Red_Close_ThreePlusNine {
                         SHOOT_HEADING, new TranslationalVelConstraint(100)
                 );
 
-
         TrajectoryActionBuilder path4 = myBot.getDrive().actionBuilder(
                         new Pose2d(SHOOT_POSE_X, SHOOT_POSE_Y, SHOOT_HEADING))
 
                 .splineToLinearHeading(
-                        new Pose2d(MID_COLLECT_POSE_X, NORTH_COLLECT_POSE_Y - 3, NORTH_HEADING),
+                        new Pose2d(MID_COLLECT_POSE_X, SOUTH_COLLECT_POSE_Y, SOUTH_HEADING),
                         new Rotation2d(0, 4)
                 )
 
                 .strafeToLinearHeading(
                         new Vector2d(SHOOT_POSE_X, SHOOT_POSE_Y),
-                        SHOOT_HEADING, new TranslationalVelConstraint(100));
+                        SHOOT_HEADING, new TranslationalVelConstraint(100))
 
+                ;
+
+        TrajectoryActionBuilder path6 = myBot.getDrive().actionBuilder(
+                        new Pose2d(MID_COLLECT_POSE_X, SOUTH_COLLECT_POSE_Y, SOUTH_HEADING))
+                .splineToLinearHeading(
+                        new Pose2d(SHOOT_POSE_X, SHOOT_POSE_Y, SHOOT_HEADING),
+                        new Rotation2d(2, 1));
 
         TrajectoryActionBuilder path7 = myBot.getDrive().actionBuilder(
                         new Pose2d(SHOOT_POSE_X, SHOOT_POSE_Y, SHOOT_HEADING))
-
                 .splineToLinearHeading(
-                        new Pose2d(RIGHT_COLLECT_POSE_X, NORTH_COLLECT_POSE_Y, NORTH_HEADING),
-                        new Rotation2d(-0.5, 3)
+                        new Pose2d(RIGHT_COLLECT_POSE_X, SOUTH_COLLECT_POSE_Y, SOUTH_HEADING),
+                        new Rotation2d(0, 3)
                 )
 
                 .strafeToLinearHeading(
                         new Vector2d(SHOOT_POSE_X, SHOOT_POSE_Y),
                         SHOOT_HEADING, new TranslationalVelConstraint(150));
 
+
+        TrajectoryActionBuilder path9 = myBot.getDrive().actionBuilder(
+                        new Pose2d(RIGHT_COLLECT_POSE_X, SOUTH_COLLECT_POSE_Y, SOUTH_HEADING))
+                .splineToLinearHeading(
+                        new Pose2d(SHOOT_POSE_X, SHOOT_POSE_Y, SHOOT_HEADING)
+                        , new Rotation2d(-1, -1));
         TrajectoryActionBuilder path10 = myBot.getDrive().actionBuilder(
                         new Pose2d(SHOOT_POSE_X, SHOOT_POSE_Y, SHOOT_HEADING))
                 .strafeToLinearHeading(
@@ -103,10 +121,13 @@ public class Red_Close_ThreePlusNine {
                 new SequentialAction(
                         new SleepAction(SHOOT_TIME),
                         path1.build(),
+//                        path3.build(),
                         new SleepAction(SHOOT_TIME),
                         path4.build(),
+//                        path6.build(),
                         new SleepAction(SHOOT_TIME),
                         path7.build(),
+//                        path9.build(),
                         new SleepAction(SHOOT_TIME),
                         path10.build()
                 )
@@ -119,3 +140,5 @@ public class Red_Close_ThreePlusNine {
                 .start();
     }
 }
+
+

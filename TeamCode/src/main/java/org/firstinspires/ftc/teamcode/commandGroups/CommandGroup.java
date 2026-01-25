836fcaa2e84d2bfa2881b0d149740a71dc7a1356
Barnyard22993 <barnyard22993@gmail.com>
2026-01-25 19:36:51 +0200

    Peter: auto 3+9 throws 9 bolls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandGroups/CommandGroup.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandGroups/CommandGroup.java
@@ -60,6 +60,8 @@ public class CommandGroup extends SequentialCommandGroup {
 
 
 
+
+
     public static void shootPreset(int n){
         if (n == 1){
                 BarnRobot.getInstance().shooterHood.setCustomPosition(0.4);

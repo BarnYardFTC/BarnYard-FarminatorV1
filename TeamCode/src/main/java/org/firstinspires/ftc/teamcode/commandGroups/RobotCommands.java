c66d581933bb99488d0095481d0db64958500982
Barnyard22993 <barnyard22993@gmail.com>
2026-02-05 17:52:11 +0200

    Sasha: working on robot commands

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandGroups/RobotCommands.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandGroups/RobotCommands.java
@@ -2,6 +2,14 @@ package org.firstinspires.ftc.teamcode.commandGroups;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.seattlesolvers.solverslib.command.Command;
+import com.seattlesolvers.solverslib.command.ParallelCommandGroup;
+import com.seattlesolvers.solverslib.command.SequentialCommandGroup;
+import com.seattlesolvers.solverslib.command.WaitCommand;
+import com.seattlesolvers.solverslib.command.WaitUntilCommand;
+
+import org.firstinspires.ftc.teamcode.BarnRobot;
+
+import java.util.function.BooleanSupplier;
 
 /**
  * A class that contains complex command groups with commands from different classes.
@@ -13,8 +21,43 @@ public class RobotCommands {
     /** Timings for intake and shooter in milliseconds. */
     public static int TRANSFER_ONE_DURATION = 1500;
     public static int TRANSFER_ALL_DURATION = TRANSFER_ONE_DURATION * 3;
-    /** Commands */
-    public static Command shootCommand(){
-        return null;
+
+    /** Shoots one artifact when shooter is ready */
+    public static Command shootOneCommand() {
+        return new SequentialCommandGroup(
+                new WaitUntilCommand(() -> BarnRobot.getInstance().shooter.isReady()),
+                BarnRobot.getInstance().intake.activateIntakeCommand(),
+                BarnRobot.getInstance().transfer.activateTransfer(),
+                new WaitCommand(TRANSFER_ONE_DURATION),
+                BarnRobot.getInstance().intake.deactivateIntakeCommand(),
+                BarnRobot.getInstance().transfer.deactivateTransfer()
+        );
+    }
+
+    /** Shoots three artifacts */
+    public static Command shootAllCommand() {
+        return new SequentialCommandGroup(
+                shootOneCommand(),
+                shootOneCommand(),
+                shootOneCommand()
+        );
+    }
+
+    /** Collects artifacts and prevents them from being shot by closing the gate */
+    public static Command collectCommand() {
+        return new ParallelCommandGroup(
+                BarnRobot.getInstance().gate.closeCommand(),
+                BarnRobot.getInstance().intake.activateIntakeCommand(),
+                BarnRobot.getInstance().transfer.activateTransfer()
+        );
+    }
+
+    /** Stops Collecting artifacts */
+    public static Command collectStopCommand() {
+        return new ParallelCommandGroup(
+                BarnRobot.getInstance().intake.deactivateIntakeCommand(),
+                BarnRobot.getInstance().transfer.deactivateTransfer(),
+                BarnRobot.getInstance().gate.openCommand()
+        );
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/GovnoTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/GovnoTesting.java
@@ -2,16 +2,21 @@ package org.firstinspires.ftc.teamcode.testing;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.seattlesolvers.solverslib.command.CommandOpMode;
+import com.seattlesolvers.solverslib.command.InstantCommand;
+import com.seattlesolvers.solverslib.gamepad.GamepadKeys;
 
 import org.firstinspires.ftc.teamcode.BarnRobot;
+import org.firstinspires.ftc.teamcode.commandGroups.RobotCommands;
 
 @TeleOp(name = "Cmd", group = "test")
-public class CmdTestTeleop extends CommandOpMode {
+public class GovnoTesting extends CommandOpMode {
     //WIP
     private BarnRobot farminator;
     @Override
     public void initialize() {
-
+        farminator.gamepadEx1.getGamepadButton(GamepadKeys.Button.X).whenPressed(
+                RobotCommands.collectCommand()
+        );
     }
     @Override
     public void run() {

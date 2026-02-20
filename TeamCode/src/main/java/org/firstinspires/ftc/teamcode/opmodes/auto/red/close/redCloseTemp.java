21903cf1c9ab353ddd9ebfa06c84f8c4bd446efd
Barnyard22993 <barnyard22993@gmail.com>
2026-02-20 04:20:48 +0200

    egor: some progress on red auto 3 + 9,still works as shit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/red/close/redCloseTemp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/red/close/redCloseTemp.java
@@ -12,23 +12,24 @@ public class redCloseTemp {
 
     public static final int SHOOT_TIME_MS = 1500;
 
-    public static final Pose2d startPose = new Pose2d( -53.333, 45.5, Math.toRadians(-225.0));
-    public static final Pose2d shootPose = new Pose2d(-23, 22.0, Math.toRadians(-224.0));
-    public static final Pose2d lastShootPose = new Pose2d(-36, 15.0, Math.toRadians(-242.0));
+
+    public static final Pose2d startPose = new Pose2d( -49.333, 45.5, Math.toRadians(360 - 225));
+    public static final Pose2d shootPose = new Pose2d(-23, 22, Math.toRadians(360 - 224));
+    public static final Pose2d lastShootPose = new Pose2d(-36, 8, Math.toRadians(360 - 240));
 
 
-    public static final Pose2d leftCollectPose = new Pose2d(-11.5, 53.0, Math.toRadians(-270.0));
-    public static final Pose2d midCollectPose = new Pose2d(12, 62.0, Math.toRadians(-270.0));
-    public static final Pose2d rightCollectPose = new Pose2d(35.5, 65.0, Math.toRadians(-270.0));
+    public static final Pose2d leftCollectPose = new Pose2d(-11.5, 58, Math.toRadians(360 - 270));
+    public static final Pose2d midCollectPose = new Pose2d(12, 72, Math.toRadians(360 - 270));
+    public static final Pose2d rightCollectPose = new Pose2d(35.5, 72, Math.toRadians(360 - 270));
 
 
-    public static final Pose2d leftLoadZoneReady = new Pose2d(-11.5, 20.0, Math.toRadians(-270.0));
-    public static final Pose2d rightLoadZoneReady = new Pose2d(12, 20.0, Math.toRadians(-270.0));
-    public static final Pose2d midLoadZoneReady = new Pose2d(34.5, 20.0, Math.toRadians(-270.0));
+    public static final Pose2d leftLoadZoneReady = new Pose2d(-11.5, 20, Math.toRadians(360 - 270));
+    public static final Pose2d rightLoadZoneReady = new Pose2d(12, 20, Math.toRadians(360 - 270));
+    public static final Pose2d midLoadZoneReady = new Pose2d(34.5, 20, Math.toRadians(360 - 270));
 
-    public static final Pose2d parkPose = new Pose2d(-40, 22.0, Math.toRadians(-227.0));
-    public static final Pose2d gateOpenPose = new Pose2d(0, 70.0, Math.toRadians(-180.0));
-    public static final Pose2d gateCollectPose = new Pose2d(10,57.0, Math.toRadians(-227.0));
+    public static final Pose2d parkPose = new Pose2d(-40, 22, Math.toRadians(360 - 227));
+    public static final Pose2d gateOpenPose = new Pose2d(0, 65, Math.toRadians(360 - 180));
+    public static final Pose2d gateCollectPose = new Pose2d(10,57, Math.toRadians(360 - 227));
 
     private static final TranslationalVelConstraint fastToShoot = new TranslationalVelConstraint(RoadRunnerMecanumDrive.PARAMS.maxWheelVel*1.5);
 
@@ -52,22 +53,23 @@ public class redCloseTemp {
                 .strafeToLinearHeading(shootPose.component1(),shootPose.component2());
         goCollectLeft = goShootPre.endTrajectory().fresh()
                 .splineToLinearHeading(leftCollectPose, new Rotation2d(-.1, 2.0))
-                .setTangent(new Rotation2d(1,-1.0))
-                .splineToLinearHeading(gateOpenPose, Math.toRadians(100.0), fastToShoot);
+                .setTangent(new Rotation2d(2,-2.0))
+                .splineToLinearHeading(gateOpenPose, Math.toRadians(100.0), fastToShoot);//
         goShootLeft = goCollectLeft.endTrajectory().fresh()
-                .strafeToLinearHeading(shootPose.component1(),shootPose.component2(), fastToShoot);
+                .strafeToLinearHeading(shootPose.component1(),shootPose.component2(), fastToShoot);//
         goCollectMid = goShootLeft.endTrajectory().fresh()
-                .setTangent(new Rotation2d(2.1,-0.4))
-                .splineToLinearHeading(midCollectPose, Math.toRadians(90.0), fastToShoot);
+                .setTangent(new Rotation2d(2.7,-0.8))
+                .splineToLinearHeading(midCollectPose, Math.toRadians(90.0), fastToShoot);//
         goShootMid = goCollectMid.endTrajectory().fresh()
                 .setTangent(new Rotation2d(0,-1.0))
                 .splineToLinearHeading(shootPose, Math.toRadians(-150.0));
         goCollectRight = goShootMid.endTrajectory().fresh()
                 .setTangent(new Rotation2d(2.1,-0.2))
-                .splineToSplineHeading(rightCollectPose, new Rotation2d(0, 1.5),
-                        new TranslationalVelConstraint(RoadRunnerMecanumDrive.PARAMS.maxWheelVel*1.25));
+                .splineToSplineHeading(rightCollectPose, new Rotation2d(0, 1.5));
+
+//                        new TranslationalVelConstraint(RoadRunnerMecanumDrive.PARAMS.maxWheelVel*1.25));
         goShootLast = goCollectRight.endTrajectory().fresh                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          ()
                 .setTangent(Math.toRadians(-120.0))
-                .splineToLinearHeading(lastShootPose, Math.toRadians(-198.0), fastToShoot);
+                .splineToLinearHeading(lastShootPose, Math.toRadians(198.0), fastToShoot);//
     }
 }

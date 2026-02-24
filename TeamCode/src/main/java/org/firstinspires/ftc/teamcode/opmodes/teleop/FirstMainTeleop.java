a40c9dc40de5cde0e5f7289b38d73258891862ba
egor <barnyard22993@gmail.com>
2026-02-24 10:30:17 +0200

    new shooterhood tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/FirstMainTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/FirstMainTeleop.java
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.seattlesolvers.solverslib.command.CommandOpMode;
 import com.seattlesolvers.solverslib.command.InstantCommand;
 import com.seattlesolvers.solverslib.command.ParallelCommandGroup;
+import com.seattlesolvers.solverslib.command.ParallelRaceGroup;
 import com.seattlesolvers.solverslib.command.RunCommand;
 import com.seattlesolvers.solverslib.command.SequentialCommandGroup;
 import com.seattlesolvers.solverslib.command.button.Trigger;
@@ -17,37 +18,38 @@ import org.firstinspires.ftc.teamcode.commandGroups.CommandGroup;
 import org.firstinspires.ftc.teamcode.util.OpModeData;
 
 import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
 
-
-@TeleOp(name = "!MAIN TELEOP", group = "!")
+@Config
+@TeleOp(name = "!!First TELEOP", group = "!")
 public class FirstMainTeleop extends CommandOpMode{
-        // ------------------------
-        // Robot Instance
-        // ------------------------
-        private BarnRobot farminator;
-        private TeleopTemplate template;
 
-        @Override
-        public void initialize() {
+    // ------------------------
+    // Robot Instance
+    // ------------------------
+    private BarnRobot farminator;
+    private TeleopTemplate template;
+    public static double pos = 0.95;
+    @Override
+    public void initialize() {
 
-            OpModeData opModeData = new OpModeData(
-                    OpModeData.AllianceColor.BLUE,
-                    OpModeData.OpModeType.TELEOP,
-                    new Pose2d(0, 0, Math.toRadians(180)),
-                    180
-            );
+        OpModeData opModeData = new OpModeData(
+                OpModeData.AllianceColor.RED,
+                OpModeData.OpModeType.TELEOP,
+                new Pose2d(0, 0, Math.toRadians(90)),
+                90
+        );
 
-            // ==========================================================
-            // Robot Initialization
-            // ==========================================================
-            farminator = BarnRobot.getInstance();
-            farminator.init(
-                    this,
-                    opModeData
-            );
-            template = new TeleopTemplate();
-            template.initControls();
-
+        // ==========================================================
+        // Robot Initialization
+        // ==========================================================
+        farminator = BarnRobot.getInstance();
+        farminator.init(
+                this,
+                opModeData
+        );
+        template = new TeleopTemplate();
+        template.initControls();
     }
 
 
@@ -55,33 +57,10 @@ public class FirstMainTeleop extends CommandOpMode{
     @Override
     public void run() {
         super.run();
-        farminator.shooterHood.displayTelemetry();
-        farminator.drive.displayPinpointDataTelemetry();
-        farminator.shooter.displayTelemetry();
-//        telemetry.addData("ang", farminator.pinpointLocalizer.getPose().heading.toDouble());
-        telemetry.addData("ang", Math.toDegrees(farminator.pinpointLocalizer.getPose().heading.toDouble()));
-//        telemetry.addData("Loop Time (ms)", getRuntime() * 1000);
-//        telemetry.addData("default drive command: ", farminator.drive.getDefaultCommand());
-        telemetry.addData("Is smart functions ON? ", farminator.colorSensor.getIntakeMode());
-        farminator.colorSensor.displayTelemetry(telemetry);
-        telemetry.addData("is in zone", farminator.drive.isInsideLaunchZone());
         farminator.limelight.displayTelemetry();
-        farminator.periodic();
-    }
-
-    private void intakeSwitcher(){
-        if (farminator.colorSensor.getIntakeMode()){
-            farminator.intake.smartIntakeCommand().schedule();
-            farminator.transfer.smartTransferCommand().schedule();
-            telemetry.addLine("\n\n\n====SMART====\n\n\n");
-        }else{
-            farminator.intake.activateIntakeCommand().schedule();
-            farminator.transfer.activateTransfer().schedule();
-            telemetry.addLine("\n\n\n====DUMB====\n\n\n");
-        }
-    }
-
-    private InstantCommand rumbleCommand() {
-        return new InstantCommand(() -> gamepad1.rumble(200));
+        telemetry.addData("dis", BarnRobot.getInstance().limelight.getGoalDistance());
+        telemetry.addData("val", BarnRobot.getInstance().shooter.getTargetVelocity());
+        farminator.shooterHood.HoodRunCommand(() -> pos).schedule();
+        telemetry.update();
     }
 }

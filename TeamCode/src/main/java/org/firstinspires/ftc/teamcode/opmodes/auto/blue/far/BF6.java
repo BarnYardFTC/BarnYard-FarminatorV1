353a10b80c0ce4fbb7e1b72f09e26715cd07d363
GassCod <sashapapa8@gmail.com>
2026-03-24 16:26:51 +0200

    Peter: returned working shhooting system

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/blue/far/BF6.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/blue/far/BF6.java
@@ -43,7 +43,7 @@ public class BF6 extends CommandOpMode {
             drive = new RoadRunnerMecanumDrive(hardwareMap, autoHub.positions.get(AutoPars.positions.START_FAR));
             BlueFarTemp.createPath(drive);
 
-            farminator.shooter.setDefaultCommand(farminator.shooter.runShooterFormulaBased());
+            farminator.shooter.setDefaultCommand(farminator.shooter.runShooterBasedOnDistance());
             farminator.shooterHood.setDefaultCommand(farminator.shooterHood.autoHoodAlignment());
 
 /**

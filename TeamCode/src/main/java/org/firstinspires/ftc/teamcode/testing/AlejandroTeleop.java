57cb4df0adc6f92fc5b8e440c136ce8016bfe41a
egor <barnyard22993@gmail.com>
2026-05-24 16:36:42 +0300

    changed name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/AlejandroTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/AlejandroTeleop.java
@@ -5,6 +5,7 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.seattlesolvers.solverslib.gamepad.GamepadEx;
@@ -26,16 +27,21 @@ public class AlejandroTeleop extends LinearOpMode {
     private static final double MIN_DRIVE_POWER = 0.2;
     private static final double MAX_DRIVE_POWER = 1.0;
 
-    private static final double MIN_INTAKE = 0.0;
-    private static final double MAX_INTAKE = 1.0;
+    private static final double INTAKE_OFF_POWER = 0.0;
+    private static final double INTAKE_ON_POWER = 1.0;
 
-    private static final double MIN_TRANSFER = 0.0;
-    private static final double MAX_TRANSFER = 1.0;
+    private static final double TRANSFER_OFF_POWER = 0.0;
+    private static final double TRANSFER_ON_POWER = 1.0;
 
     private static final double MIN_SHOOTER_HOOD = 0.35;
     private static final double MAX_SHOOTER_HOOD = 0.95;
 
+    private static final double GATE_OPEN = 0.88;
+    private static final double GATE_CLOSED = 1.00;
+
     private static final double STRAFE_COMPENSATION = 1.1;
+
+    private static final double SHOOTER_VELOCITY = 800;
 
     private DcMotor leftFront;
     private DcMotor rightFront;
@@ -43,8 +49,13 @@ public class AlejandroTeleop extends LinearOpMode {
     private DcMotor rightBack;
 
     private DcMotor intake;
-
     private DcMotor transfer;
+
+    private Servo leftGate;
+    private Servo rightGate;
+
+    private DcMotor shooterLeft;
+    private DcMotor shooterRight;
 
     private IMU imu;
     private Servo shooterHood;
@@ -55,14 +66,17 @@ public class AlejandroTeleop extends LinearOpMode {
     private double y;
     private double rx;
 
-    private double drivetrainPower = 1;
+    private double drivetrainPower = MAX_DRIVE_POWER;
 
-    private double currentShooterHood = 0;
+    private double currentShooterHood = MIN_SHOOTER_HOOD;
 
     private boolean fieldCentricEnabled = false;
 
     private boolean isIntake = false;
     private boolean isTransfer = false;
+    private boolean isShooter = false;
+
+    private boolean gateOpen = false;
 
     @Override
     public void runOpMode() {
@@ -74,8 +88,13 @@ public class AlejandroTeleop extends LinearOpMode {
 
         while (opModeIsActive()) {
             updateInput();
+
             updateDrive();
+            updateIntake();
+            updateTransfer();
+            updateShooter();
             updateServos();
+
             updateTelemetry();
         }
     }
@@ -91,19 +110,33 @@ public class AlejandroTeleop extends LinearOpMode {
         leftBack.setDirection(DcMotor.Direction.REVERSE);
 
         intake = hardwareMap.get(DcMotor.class, "intake");
-
         transfer = hardwareMap.get(DcMotor.class, "transfer");
+
+        leftGate = hardwareMap.get(Servo.class, "leftGate");
+        rightGate = hardwareMap.get(Servo.class, "rightGate");
+
+        leftGate.setDirection(Servo.Direction.REVERSE);
+        rightGate.setDirection(Servo.Direction.FORWARD);
+
+        shooterLeft = hardwareMap.get(DcMotor.class, "shooterLeft");
+        shooterRight = hardwareMap.get(DcMotor.class, "shooterRight");
+
+        shooterLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        shooterLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        shooterLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        shooterRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        shooterRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        shooterRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        shooterHood = hardwareMap.get(Servo.class, "shooterHood");
+        shooterHood.setDirection(Servo.Direction.REVERSE);
 
         imu = hardwareMap.get(IMU.class, "imu");
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
                 RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
                 RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
         imu.initialize(parameters);
-
-        shooterHood = hardwareMap.get(Servo.class, "shooterHood");
-
-        shooterHood.setDirection(Servo.Direction.REVERSE);
-        shooterHood.scaleRange(0,1);
 
         gamepad = new GamepadEx(this.gamepad1);
     }
@@ -127,7 +160,7 @@ public class AlejandroTeleop extends LinearOpMode {
             else drivetrainPower = MAX_DRIVE_POWER;
         }
 
-        if(gamepad.wasJustPressed(GamepadKeys.Button.RIGHT_BUMPER)) {
+        if (gamepad.wasJustPressed(GamepadKeys.Button.RIGHT_BUMPER)) {
             if(currentShooterHood - 0.1 > MIN_SHOOTER_HOOD) currentShooterHood -= 0.1;
             else currentShooterHood = MIN_SHOOTER_HOOD;
         } else if (gamepad.wasJustPressed(GamepadKeys.Button.LEFT_BUMPER)) {
@@ -135,9 +168,17 @@ public class AlejandroTeleop extends LinearOpMode {
             else currentShooterHood = MAX_SHOOTER_HOOD;
         }
 
-        if(gamepad.wasJustPressed(GamepadKeys.Button.DPAD_UP)) {
+        if (gamepad.wasJustPressed(GamepadKeys.Button.DPAD_UP)) {
             isIntake = !isIntake;
             isTransfer = !isTransfer;
+        }
+
+        if (gamepad.wasJustPressed(GamepadKeys.Button.DPAD_LEFT)) {
+            gateOpen = !gateOpen;
+        }
+
+        if (gamepad.wasJustPressed(GamepadKeys.Button.DPAD_DOWN)) {
+            isShooter = !isShooter;
         }
 
         if (gamepad.wasJustPressed(GamepadKeys.Button.Y)) {
@@ -171,21 +212,41 @@ public class AlejandroTeleop extends LinearOpMode {
         double rightFrontPower = (driveY - driveX - rx) / denominator;
         double rightBackPower = (driveY + driveX - rx) / denominator;
 
-        intake.setPower(isIntake ? MAX_INTAKE : MIN_INTAKE);
-        transfer.setPower(isTransfer ? MAX_TRANSFER : MIN_TRANSFER);
-
         setDrivePower(leftFrontPower * drivetrainPower, rightFrontPower * drivetrainPower, leftBackPower * drivetrainPower, rightBackPower * drivetrainPower);
+    }
+
+    private void updateIntake() {
+        intake.setPower(isIntake ? INTAKE_ON_POWER : INTAKE_OFF_POWER);
+    }
+
+    private void updateTransfer() {
+        transfer.setPower(isTransfer ? TRANSFER_ON_POWER : TRANSFER_OFF_POWER);
+    }
+
+    private void updateShooter() {
+        shooterLeft.setPower(isShooter ? SHOOTER_VELOCITY : 0);
+        shooterRight.setPower(isShooter ? SHOOTER_VELOCITY : 0);
     }
 
     private void updateServos() {
         shooterHood.setPosition(currentShooterHood);
+
+        leftGate.setPosition(gateOpen ? GATE_OPEN : GATE_CLOSED);
+        rightGate.setPosition(gateOpen ? GATE_OPEN : GATE_CLOSED);
     }
 
     private void updateTelemetry() {
         telemetry.addData("X", x);
         telemetry.addData("Y", y);
+
         telemetry.addData("isFieldCentricDrive", fieldCentricEnabled);
+
         telemetry.addData("Shooter Hood", currentShooterHood);
+
+        telemetry.addData("Intake", isIntake);
+        telemetry.addData("Transfer", isTransfer);
+        telemetry.addData("Gate", gateOpen);
+        telemetry.addData("Shooter", isShooter);
 
         telemetry.update();
     }

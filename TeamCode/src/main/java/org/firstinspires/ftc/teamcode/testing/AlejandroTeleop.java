d016e0dd244ea28b4177ccdf5bdfc9da42a25307
egor <barnyard22993@gmail.com>
2026-05-24 15:48:25 +0300

    changed name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/AlejandroTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/AlejandroTeleop.java
@@ -22,102 +22,187 @@ Tasks:
  */
 @TeleOp(name = "AlejandroTeleop", group = "Aboba")
 public class AlejandroTeleop extends LinearOpMode {
-    Servo servo1;
-    GamepadEx gamepad;
+
+    private static final double MIN_DRIVE_POWER = 0.2;
+    private static final double MAX_DRIVE_POWER = 1.0;
+
+    private static final double MIN_INTAKE = 0.0;
+    private static final double MAX_INTAKE = 1.0;
+
+    private static final double MIN_TRANSFER = 0.0;
+    private static final double MAX_TRANSFER = 1.0;
+
+    private static final double MIN_SHOOTER_HOOD = 0.35;
+    private static final double MAX_SHOOTER_HOOD = 0.95;
+
+    private static final double STRAFE_COMPENSATION = 1.1;
+
+    private DcMotor leftFront;
+    private DcMotor rightFront;
+    private DcMotor leftBack;
+    private DcMotor rightBack;
+
+    private DcMotor intake;
+
+    private DcMotor transfer;
+
+    private IMU imu;
+    private Servo shooterHood;
+
+    private GamepadEx gamepad;
+
+    private double x;
+    private double y;
+    private double rx;
+
+    private double drivetrainPower = 1;
+
+    private double currentShooterHood = 0;
+
+    private boolean fieldCentricEnabled = false;
+
+    private boolean isIntake = false;
+    private boolean isTransfer = false;
 
     @Override
     public void runOpMode() {
-        DcMotor leftFrontDrivetrain = hardwareMap.get(DcMotor.class, "leftFrontDrivetrain");
-        DcMotor rightFrontDrivetrain = hardwareMap.get(DcMotor.class, "rightFrontDrivetrain");
-        DcMotor leftBackDrivetrain = hardwareMap.get(DcMotor.class, "leftBackDrivetrain");
-        DcMotor rightBackDrivetrain = hardwareMap.get(DcMotor.class, "rightBackDrivetrain");
-
-        leftFrontDrivetrain.setDirection(DcMotor.Direction.REVERSE);
-        leftBackDrivetrain.setDirection(DcMotor.Direction.REVERSE);
-
-        IMU imu = hardwareMap.get(IMU.class, "imu");
-        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
-                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
-        imu.initialize(parameters);
-
-        servo1 = hardwareMap.get(Servo.class, "shooterHood");
-
-        gamepad = new GamepadEx(this.gamepad1);
-
-        servo1.setDirection(Servo.Direction.REVERSE);
-        servo1.scaleRange(0,1);
-
-        double x, y, rx,
-                leftFrontDrivetrainPower, rightFrontDrivetrainPower, leftBackDrivetrainPower, rightBackDrivetrainPower,
-                drivetrainPower = 1, minDrivetrainPower = 0.2, maxDrivetrainPower = 1,
-                denominator, botHeading, rotX, rotY;
-
-        boolean isFieldCentricDrive = false;
+        initializeHardware();
 
         waitForStart();
 
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            y = gamepad.getLeftY();
-            x = gamepad.getLeftX();
-            rx = gamepad.getRightX();
-
-            if(gamepad.wasJustPressed(GamepadKeys.Button.X))
-                isFieldCentricDrive = !isFieldCentricDrive;
-
-            if(gamepad.wasJustPressed(GamepadKeys.Button.A)) {
-                if(drivetrainPower - 0.1 > minDrivetrainPower) drivetrainPower -= 0.1;
-                else drivetrainPower = minDrivetrainPower;
-            } else if (gamepad.wasJustPressed(GamepadKeys.Button.B)) {
-                if(drivetrainPower + 0.1 < maxDrivetrainPower) drivetrainPower += 0.1;
-                else drivetrainPower = maxDrivetrainPower;
-            }
-
-            if (gamepad.wasJustPressed(GamepadKeys.Button.OPTIONS)) {
-                imu.resetYaw();
-            }
-
-            telemetry.addData("X", x);
-            telemetry.addData("Y", y);
-            telemetry.addData("isFieldCentricDrive", isFieldCentricDrive);
-
-            if(isFieldCentricDrive) {
-
-                botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-
-                rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
-                rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
-
-                telemetry.addData("rotX", rotX);
-                telemetry.addData("rotY", rotY);
-
-                x = rotX * 1.1;
-                y = rotY;
-            }
-
-            denominator = Math.max(Math.abs(x) + Math.abs(y) + Math.abs(rx), 1);
-
-            leftFrontDrivetrainPower = (y + x + rx) / denominator;
-            leftBackDrivetrainPower = (y - x + rx) / denominator;
-            rightFrontDrivetrainPower = (y - x - rx) / denominator;
-            rightBackDrivetrainPower = (y + x - rx) / denominator;
-
-            /*if (gamepad.wasJustPressed(GamepadKeys.Button.X)) {
-                servoUp();
-            } else if (gamepad.wasJustPressed(GamepadKeys.Button.Y)) {
-                servoDown();
-            }*/
-
-            leftFrontDrivetrain.setPower(leftFrontDrivetrainPower * drivetrainPower);
-            leftBackDrivetrain.setPower(leftBackDrivetrainPower * drivetrainPower);
-            rightFrontDrivetrain.setPower(rightFrontDrivetrainPower * drivetrainPower);
-            rightBackDrivetrain.setPower(rightBackDrivetrainPower * drivetrainPower);
-
-            telemetry.update();
-
-            gamepad.readButtons();
+            updateInput();
+            updateDrive();
+            updateServos();
+            updateTelemetry();
         }
+    }
+
+    private void initializeHardware() {
+
+        leftFront = hardwareMap.get(DcMotor.class, "leftFrontDrivetrain");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFrontDrivetrain");
+        leftBack = hardwareMap.get(DcMotor.class, "leftBackDrivetrain");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBackDrivetrain");
+
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        leftBack.setDirection(DcMotor.Direction.REVERSE);
+
+        intake = hardwareMap.get(DcMotor.class, "intake");
+
+        transfer = hardwareMap.get(DcMotor.class, "transfer");
+
+        imu = hardwareMap.get(IMU.class, "imu");
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+        imu.initialize(parameters);
+
+        shooterHood = hardwareMap.get(Servo.class, "shooterHood");
+
+        shooterHood.setDirection(Servo.Direction.REVERSE);
+        shooterHood.scaleRange(0,1);
+
+        gamepad = new GamepadEx(this.gamepad1);
+    }
+
+    private void updateInput() {
+
+        gamepad.readButtons();
+
+        y = applyDeadzone(gamepad.getLeftY());
+        x = applyDeadzone(gamepad.getLeftX());
+        rx = applyDeadzone(gamepad.getRightX());
+
+        if(gamepad.wasJustPressed(GamepadKeys.Button.X))
+            fieldCentricEnabled = !fieldCentricEnabled;
+
+        if(gamepad.wasJustPressed(GamepadKeys.Button.A)) {
+            if(drivetrainPower - 0.1 > MIN_DRIVE_POWER) drivetrainPower -= 0.1;
+            else drivetrainPower = MIN_DRIVE_POWER;
+        } else if (gamepad.wasJustPressed(GamepadKeys.Button.B)) {
+            if(drivetrainPower + 0.1 < MAX_DRIVE_POWER) drivetrainPower += 0.1;
+            else drivetrainPower = MAX_DRIVE_POWER;
+        }
+
+        if(gamepad.wasJustPressed(GamepadKeys.Button.RIGHT_BUMPER)) {
+            if(currentShooterHood - 0.1 > MIN_SHOOTER_HOOD) currentShooterHood -= 0.1;
+            else currentShooterHood = MIN_SHOOTER_HOOD;
+        } else if (gamepad.wasJustPressed(GamepadKeys.Button.LEFT_BUMPER)) {
+            if(currentShooterHood + 0.1 < MAX_SHOOTER_HOOD) currentShooterHood += 0.1;
+            else currentShooterHood = MAX_SHOOTER_HOOD;
+        }
+
+        if(gamepad.wasJustPressed(GamepadKeys.Button.DPAD_UP)) {
+            isIntake = !isIntake;
+            isTransfer = !isTransfer;
+        }
+
+        if (gamepad.wasJustPressed(GamepadKeys.Button.Y)) {
+            imu.resetYaw();
+        }
+    }
+
+    private void updateDrive() {
+
+        double driveX = x;
+        double driveY = y;
+
+        if(fieldCentricEnabled) {
+
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+            double rotX = driveX * Math.cos(-botHeading) - driveY * Math.sin(-botHeading);
+            double rotY = driveX * Math.sin(-botHeading) + driveY * Math.cos(-botHeading);
+
+            telemetry.addData("rotX", rotX);
+            telemetry.addData("rotY", rotY);
+
+            driveX = rotX * STRAFE_COMPENSATION;
+            driveY = rotY;
+        }
+
+        double denominator = Math.max(Math.abs(driveX) + Math.abs(driveY) + Math.abs(rx), 1);
+
+        double leftFrontPower = (driveY + driveX + rx) / denominator;
+        double leftBackPower = (driveY - driveX + rx) / denominator;
+        double rightFrontPower = (driveY - driveX - rx) / denominator;
+        double rightBackPower = (driveY + driveX - rx) / denominator;
+
+        intake.setPower(isIntake ? MAX_INTAKE : MIN_INTAKE);
+        transfer.setPower(isTransfer ? MAX_TRANSFER : MIN_TRANSFER);
+
+        setDrivePower(leftFrontPower * drivetrainPower, rightFrontPower * drivetrainPower, leftBackPower * drivetrainPower, rightBackPower * drivetrainPower);
+    }
+
+    private void updateServos() {
+        shooterHood.setPosition(currentShooterHood);
+    }
+
+    private void updateTelemetry() {
+        telemetry.addData("X", x);
+        telemetry.addData("Y", y);
+        telemetry.addData("isFieldCentricDrive", fieldCentricEnabled);
+        telemetry.addData("Shooter Hood", currentShooterHood);
+
+        telemetry.update();
+    }
+
+    private void setDrivePower(
+            double lf,
+            double rf,
+            double lb,
+            double rb
+    ) {
+        leftFront.setPower(lf);
+        rightFront.setPower(rf);
+        leftBack.setPower(lb);
+        rightBack.setPower(rb);
+    }
+
+    private double applyDeadzone(double value) {
+        return Math.abs(value) > 0.05 ? value : 0;
     }
 }

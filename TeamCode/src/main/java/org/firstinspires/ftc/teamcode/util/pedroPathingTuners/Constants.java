3e5a7f9027f5823c30f7d7557c5f6f5f9abc6f1e
Sasha <barnyard22993@gmail.com>
2026-05-31 17:55:27 +0300

    working on pepe (pedro pathing), currently on pinpoint stage

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/pedroPathingTuners/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/pedroPathingTuners/Constants.java
@@ -3,11 +3,28 @@ package org.firstinspires.ftc.teamcode.util.pedroPathingTuners;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
+
+    private static final double ROBOT_MASS_KG = 5;
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(ROBOT_MASS_KG);
+
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(1)
+            .rightFrontMotorName("rightFrontDrivetrain")
+            .rightRearMotorName("rightBackDrivetrain")
+            .leftRearMotorName("leftBackDrivetrain")
+            .leftFrontMotorName("leftFrontDrivetrain")
+            .leftFrontMotorDirection(DcMotorEx.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorEx.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorEx.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorEx.Direction.FORWARD);
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
